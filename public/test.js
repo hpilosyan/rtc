11757b90f45628585349246813885d84f0f48a54
Hayk Pilosyan <hpilosyan@macadamian.com>
2014-03-13 23:15:52 +0400

    STUN/TURN

--- public/test.js
+++ public/test.js
@@ -11,7 +11,23 @@ var localVideo = $("#local_video")[0];
 var remoteVideo = $("#remote_video")[0];
 
 var pc;
-var ice = { "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] };
+var ice = {
+  'iceServers': [
+    {
+      'url': 'stun:stun.l.google.com:19302'
+    },
+    {
+      'url': 'turn:192.158.29.39:3478?transport=udp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    },
+    {
+      'url': 'turn:192.158.29.39:3478?transport=tcp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    }
+  ]
+};
 var sdpConstraints = {'mandatory': {
                         'OfferToReceiveAudio':true,
                         'OfferToReceiveVideo':true }
