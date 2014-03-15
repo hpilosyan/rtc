5fe907c13b642fce3effada263fead1b2fb3f34b
Hayk Pilosyan <hpilosyan@macadamian.com>
2014-03-15 20:38:44 +0400

    Clean up

--- server.js
+++ server.js
@@ -139,12 +139,3 @@ io.sockets.on('connection', function(socket) {
         }
     });
 });
-
-
-// now, it's easy to send a message to just the clients in a given room
-/*room = "abc123";
-io.sockets.in(room).emit('message', 'what is going on, party people?');
-
-// this message will NOT go to the client defined above
-io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
-*/
